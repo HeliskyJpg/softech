3771de1a63fab210420f359e228732f3d293dd50
Hely Mendez <hmendezm6@miumg.edu.gt>
2026-09-19 11:55:32 -0600

    feat: agrega funcionalidad listar proveedores en home.html -registra ruta en controlador -crea template base -agrega estilos a home.html para listar datos de proveedores

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ load_dotenv()
 app = Flask(__name__)
 
 app.secret_key = user=os.getenv("SECRET_KEY")
+
+@app.route("/")
+def index():
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute("SELECT * FROM productos ORDER BY id DESC")
+    productos = cur.fetchall()
+    cur.close(); conn.close()
+    return render_template("home.html", productos=productos)
+
+if __name__ == "__main__":
+    app.run(debug=True)
